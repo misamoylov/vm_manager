b06373db69f24b0a3f086cdca48b54e92ff324bf
Mikhail Samoylov <m.samoylov@postgrespro.ru>
2016-09-22 12:55:15 +0300

    Refactor

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 
 setup(
     name='vm_manager',
-    version='1.4',
+    version='0.1',
     packages=['vm_manager'],
     url='https://github.com/misamoylov/vm_manager',
     license='GPL',
