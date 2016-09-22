8aba36798c46b9bacf5f45bf077fe3ad9d25f087
Mikhail Samoylov <m.samoylov@postgrespro.ru>
2016-09-22 12:30:37 +0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+from distutils.core import setup
+
+setup(
+    name='vm_manager',
+    version='1.4',
+    packages=['vm_manager'],
+    url='https://github.com/misamoylov/vm_manager',
+    license='GPL',
+    author='Mikhail Samoylov',
+    author_email='mikhailsamoiloff@gmail.com',
+    description='VM Manager',
+)
