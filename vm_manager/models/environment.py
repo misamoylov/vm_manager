140e5754631a65af7bf7daaa0c849b6136fcdc0d
Mikhail Samoylov <m.samoylov@postgrespro.ru>
2016-09-27 18:11:47 +0300

    Add methods for vm management

--- vm_manager/models/environment.py
+++ vm_manager/models/environment.py
@@ -1,7 +1,5 @@
 import libvirt
 import os
-from xml.dom import minidom
-import sys
 import shutil
 import subprocess
 
@@ -11,10 +9,14 @@ from vm_manager.settings import IMAGES_WORKING_DIRECTORY
 
 from vm_manager.drivers.libvirt.libvirt_xml import LibvirtXmlManager as XMLManager
 
+
 class Environment(object):
     def __init__(self):
         self.conn = libvirt.open('qemu:///system')
         self.xml_manager = XMLManager()
+
+    def get_image_name(self, full_image_name):
+        return full_image_name.rstrip('.qcow2')
 
     def vm_conn(self, vm_id):
         return self.conn.lookupByID(vm_id)
@@ -38,6 +40,9 @@ class Environment(object):
         :return: list of virtual machines id
         """
         return [i.ID() for i in self.conn.listAllDomains()]
+
+    def get_vm_config(self, vm_id):
+        pass
 
     def get_vm_mac(self, vm_id):
         """Connect to vm and return their mac address
@@ -78,6 +83,7 @@ class Environment(object):
         for config in configs:
             self.conn.createXML(config)
 
+
     def prepare_vm(self, vm_ip):
         pass
 
@@ -111,4 +117,58 @@ class Environment(object):
         """
         self.vm_conn(vm_id).resume()
 
+    def start(self, vm_id):
+        """
+        Start VM.
+        """
+        if self.vm_status(vm_id) is False:
+            self.vm_conn(vm_id).create()
 
+    def vm_status(self, vm_id):
+        """Property to check if VM is active.
+         :return: if VM is active.
+        :rtype: Boolean
+        """
+        return bool(self.vm_conn(vm_id).isActive())
+
+    def suspend(self, vm_id):
+        """
+        Suspend VM.
+        """
+        if self.vm_status(vm_id):
+            self.vm_conn(vm_id).suspend()
+
+    def resume(self, vm_id):
+        """
+        Resume VM.
+        """
+        if self.is_active:
+            self.vm_conn(vm_id).resume()
+
+    def reboot(self, vm_id):
+        """
+        Reboot VM.
+        """
+        if self.vm_status(vm_id):
+            self.vm_conn(vm_id).reboot()
+
+    def shutdown(self, vm_id):
+        """
+        Shutdown VM.
+        """
+        if self.vm_status(vm_id):
+            self.vm_conn(vm_id).shutdown()
+
+    def reset(self, vm_id):
+        """
+        Reset VM.
+        """
+        if self.vm_status(vm_id):
+            self.vm_conn(vm_id).reset()
+
+    def stop(self, vm_id):
+        """
+        Stop VM.
+        """
+        if self.vm_status(vm_id):
+            self.vm_conn(vm_id).destroy()
