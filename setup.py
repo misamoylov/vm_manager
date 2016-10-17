0083983987ab1bed399d1e2130bc446fd98cf6b4
Mikhail Samoylov <m.samoylov@postgrespro.ru>
2016-10-17 12:45:12 +0300

    Some edits

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='vm_manager',
-    version='0.3',
+    version='0.4',
     packages=find_packages(),
     url='https://github.com/misamoylov/vm_manager',
     license='GPL',
